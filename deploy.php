0c2ab74ea66dc6f8ea647ce20e2285949b913a04
Andreas Ek <andreas@elseif.se>
2017-11-19 14:33:26 +0100

    Deploy script

--- deploy.php
+++ deploy.php
@@ -16,26 +16,27 @@ host('45.63.41.239')
     ->user('forge')
     ->set('branch', 'master')
     ->stage('production')
-    ->set('deploy_path', '~/')
+    ->set('deploy_path', '~/default')
     ->identityFile('~/.ssh/id_rsa');
+
 
 task('pull', function () {
 
-    $host = Context::get()->getHost();
+    $host = \Deployer\Task\Context::get()->getHost();
     $user = 'forge';
     $hostname = $host->getHostname();
     $localHostname = str_replace('.se', '.app', $hostname);
 
     $actions = [
-        "ssh deploy@{$hostname} 'cd {{deploy_path}}/current && mysqldump --user='swerob' --password='Arkitekt17!' --skip-lock-tables --hex-blob --single-transaction swerob | gzip' > db.sql.gz",
+        "ssh $user@{$hostname} 'cd {{deploy_path}}/current && mysqldump --user='forge' --password='pass' --skip-lock-tables --hex-blob --single-transaction wordcamp | gzip' > db.sql.gz",
         "gzip -df db.sql.gz",
         "wp db import db.sql",
         "rm -f db.sql",
-        "wp search-replace 'grp.elseif.se' 'grp.swerob.app' --all-tables",
+        "wp search-replace '45.63.41.239' 'wordcamp.app' --all-tables",
         "rsync --exclude .cache -re ssh " .
-        "deploy@{$hostname}:{{deploy_path}}/shared/web/app/uploads web/app",
-        "wp plugin activate query-monitor",
-        "wp plugin deactivate nginx-cache --network",
+        "$user@{$hostname}:{{deploy_path}}/shared/web/app/uploads web/app",
+        //"wp plugin activate query-monitor",
+        "wp user update 1 --user_password='admin'",
         "wp rewrite flush"
     ];
 
